5f01ddbeb8792ade93f98f36655ae0e1bb7e58f6
Ishan Gill <63602807+solarpy@users.noreply.github.com>
2023-01-09 02:03:27 +0800

    Finally Somewhat working

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger 
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other words, form data:
@@ -12,21 +33,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //serve static files
-app.use(express.static(path.join('C:\Users\Admin\Documents\GitHub\Hackathon-Project', 'Frontend')));
-
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/assets', express.static(path.join(__dirname, '../Frontend/assets')));
 
 app.get('/login(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, '..', 'Frontend', 'pages-login.html'));
 });
 
 app.get('/', (req, res) => {
-    res.redirect(301, '/login.html'); //302 by default 
+    res.redirect(301, './login.html'); //302 by default 
 });
 
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, '..', 'Frontend' ,'pages-error-404.html'));
+        res.sendFile(path.join(__dirname, '..', 'Frontend', 'pages-error-404.html'));
     }
     else if (req.accepts('json')) {
         res.json({ error: "404 Not Found"});
@@ -34,6 +56,8 @@ app.all('*', (req, res) => {
         res.type('txt').send("404 Not Found");
     }  
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
